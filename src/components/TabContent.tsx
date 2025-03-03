d9a0840c2477d816ad0a7dc5062ddee0bf9feaaf
hyerimRoh <mtxpvksh@gmail.com>
2025-03-04 02:26:13 +0900

    Fix. 탭 전환 시 컴포넌트가 매번 새로 생성되도록 변경 - 컴포넌트의 데이터 초기화를 위함

--- src/components/TabContent.tsx
+++ src/components/TabContent.tsx
@@ -1,6 +1,7 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { SwiperClass } from "swiper/react";
 import { GNBs } from "../data/Gnbs";
+import { useState } from "react";
 /* 
   탭 콘텐트(내용) 컴포넌트 
   주기능: 카테고리에 해당하는 콘텐츠를 보여줌
@@ -10,6 +11,8 @@ interface Props {
   setActiveTabIndex: React.Dispatch<React.SetStateAction<number>>;
 }
 const TabContent = ({ setSwiper, setActiveTabIndex }: Props) => {
+  const [resetKey, setResetKey] = useState(0); // 데이터 초기화를 위한 key
+
   return (
     <main className="main__container">
       <Swiper
@@ -20,13 +23,14 @@ const TabContent = ({ setSwiper, setActiveTabIndex }: Props) => {
         onSlideChange={(e) => {
           setActiveTabIndex(e.activeIndex);
           window.scrollTo(0, 0);
+          setResetKey((prev) => prev + 1); // id가 매번 달라지게 하여 새 컴포넌트로 만들기 위함
         }}
       >
         {GNBs.map((gnbItem) => {
           const Content = gnbItem.content;
           return (
             <SwiperSlide key={gnbItem.id}>
-              <Content></Content>
+              <Content key={`${gnbItem.id}-${resetKey}`}></Content>
             </SwiperSlide>
           );
         })}
